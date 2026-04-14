e41d6d977ba05a5c06c5cf7c650b8165b97ecba6
Arthur Ferraz <axrthrz1n@gmail.com>
2026-04-14 16:01:19 -0300

    Update author name in main.cpp

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
  * CURSO: CIÊNCIA DA COMPUTAÇÃO
  * DISCIPLINA: ALGORITMOS E PROGRAMAÇÃO
  * AUTORES: [Arthur Ferraz de Aquino]
- * [Vicenzo Radin]
+ * [Vicenzo Roman Radin]
  * DATA: 14/04/2026
  * PROJETO: RPG LIVRO-JOGO "ESTAÇÃO ATLAS"
  ******************************************************************************/
